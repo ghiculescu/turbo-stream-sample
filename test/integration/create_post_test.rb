c9ac6b72281d949db3933049b36892ace94eb69b
Alex Ghiculescu <alex@tanda.co>
2022-08-03 10:45:18 -0500

    write the test to match the requests the browser makes

--- test/integration/create_post_test.rb
+++ test/integration/create_post_test.rb
@@ -4,16 +4,20 @@ require 'test_helper'
 
 class CreatePostTest < ActionDispatch::IntegrationTest
   test 'cannot create an invalid post' do
-    post posts_path(format: :turbo_stream),
-          params: { post: { title: '', body: '' } }
+    post posts_path, params: { post: { title: '', body: '' } }, headers: turbo_stream
 
     assert_response 422
   end
 
   test 'create a valid post' do
-    post posts_path(format: :turbo_stream),
-          params: { post: { title: 'Some title', body: 'Some text' } }
+    post posts_path, params: { post: { title: 'Some title', body: 'Some text' } }, headers: turbo_stream
 
     assert_response 200
+  end
+
+  private
+
+  def turbo_stream
+    { "Accept" => "text/vnd.turbo-stream.html, text/html, application/xhtml+xml" }
   end
 end
